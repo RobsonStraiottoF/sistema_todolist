c2df00a19a1f80f4a1ac43ca7db897f2803981d6
robson straiotto <robson.straiotto@gmail.com>
2024-11-28 16:54:11 -0300

    ss

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('home');
-});
+use App\Http\Controllers\SiteController;
+Route::get("/",[SiteController::class,"index"]);
